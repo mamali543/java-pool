44ec865da59f6266f10dfc4a4ebc3bdefa729d1c
mamali543 <amalyreda@gmail.com>
2024-04-30 12:38:24 +0100

    day02 done

--- day02/src/ex02/Program.java
+++ day02/src/ex02/Program.java
@@ -1,6 +1,8 @@
 package ex02;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 // 1 kb = 1024 byte
 // 1 mb = 1024 * 1024 byte = 1024KB
@@ -22,11 +24,44 @@ public class Program {
         Scanner scanner = new Scanner(System.in);
         String str;
         while ((str = scanner.nextLine()) != "exit"){
-            s = str.split("//s+");
+            s = str.split("\\s+");
             if (s.length == 1 && s[0].equals("ls")){
                 listFiles(currentPosition);
+            } else if (s.length == 2 && s[0].equals("cd")) {
+                currentPosition = changeDirector(currentPosition, s);
+                System.out.println(currentPosition);
+            } else if (s.length == 3 && s[0].equals("mv")) {
+                moveFile(currentPosition, s);
             }
         }
+    }
+
+    private static String changeDirector(String currentPosition, String[] s) {
+        String newPathSegment = s[1];
+        Path currentPath = Paths.get(currentPosition);
+        Path newPath = currentPath.resolve(newPathSegment).normalize();
+
+        String normalizedPath = newPath.toString();
+
+        // Check if the resulting path is valid and exists
+        if (new File(normalizedPath).exists())
+            return normalizedPath;
+        return null;
+    }
+
+    private static void moveFile(String currentPosition, String[] s) {
+        String source = s[1];
+        String destination = s[2];
+//        System.out.println("destination: "+ destination);
+        File srcFile = new File(currentPosition,source);
+        File destFile = new File(currentPosition,destination);
+        if (destFile.exists()){
+                //if u need to move a file to a directory without specifying its new name renameTo() needs to know the destination with the name of the sourceFile
+                File newDestFile = new File(destFile, srcFile.getName());
+                srcFile.renameTo(newDestFile);
+        }
+        else
+            srcFile.renameTo(destFile);
     }
 
     private static void listFiles(String currentPosition) {
